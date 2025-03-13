6d19ba973dac4dcaef858d4fddea1782ce8454d2
causicb <benjo1306@gmail.com>
2025-03-13 19:59:22 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        // #region First task
+        //casting example
+        int a = 5;
+        int b = 2;
+        double result = (double) a / b;
+        System.out.println(result);
+
+        //variables
+        double absExample = -3.5;
+        int powExample = 2;
+        double roundExample = 3.48;
+        int sqrtExample = 4;
+
+        //various examples
+        System.out.println(Math.abs(absExample));
+        System.out.println(Math.pow(powExample, 2));
+        System.out.println(Math.round(roundExample));
+        System.out.println(Math.sqrt(sqrtExample));
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        // #endregion
+    }
+}
